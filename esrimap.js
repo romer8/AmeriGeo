6767399f81fae772b5732dc78b1e8fbfd8818248
Jason Biesinger <36056108+jasonbiesinger@users.noreply.github.com>
2019-05-23 12:16:17 -0600

    Update esrimap.js

--- esrimap.js
+++ esrimap.js
@@ -39,4 +39,4 @@ map.layers.add(Utah_Counties);
 });
 
 /*$(document).on('click', '#choose_region', zoom_to_region)
-document.getElementById("choose_region").addEventListener("click", zoom_to_region);
+document.getElementById("choose_region").addEventListener("click", zoom_to_region);*/
