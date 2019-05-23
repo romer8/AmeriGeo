86aab79862777e8f653ec7631e0e393a96e2f9b0
Jason Biesinger <36056108+jasonbiesinger@users.noreply.github.com>
2019-05-23 11:32:25 -0600

    Update esrimap.js

--- esrimap.js
+++ esrimap.js
@@ -8,23 +8,33 @@ require([
     basemap: "hybrid"
   });
   
+function zoom_to_region() {
+  var region = document.getElementById("mySelect").options;
+  if (region = "costarica") {
+      var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [-87, 13],
+      zoom: 8
+  });
+ 
+  alert("Region = " + region);
+}  
+  
   var Snotel_Network = new MapImageLayer({
   	url: "http://geoserver2.byu.edu/arcgis/rest/services/The_Snowmen/Snotel_Network/MapServer"
   });
   var Utah_Counties = new MapImageLayer({
   	url: "http://geoserver2.byu.edu/arcgis/rest/services/The_Snowmen/Utah_Counties/MapServer"
   });
-  var Utah_DEM = new MapImageLayer({
-  	url: "http://geoserver2.byu.edu/arcgis/rest/services/The_Snowmen/Utah_DEM/MapServer"
-  });
-  var Utah_State = new MapImageLayer({
-  	url: "http://geoserver2.byu.edu/arcgis/rest/services/The_Snowmen/Utah_State/MapServer"
-  });
     
+  
+  
 map.layers.add(Snotel_Network);
 map.layers.add(Utah_Counties);
 map.layers.add(Utah_DEM);
 map.layers.add(Utah_State);
+  
   var view = new MapView({
       container: "viewDiv",
       map: map,
