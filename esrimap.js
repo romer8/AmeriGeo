296fc24aa02b7441346d17f7e4a4e67150415969
Jason Biesinger <36056108+jasonbiesinger@users.noreply.github.com>
2019-05-30 09:43:10 -0600

    Update esrimap.js

--- esrimap.js
+++ esrimap.js
@@ -263,7 +263,7 @@ require([
 	  onClick: function(){
       var region = document.getElementById("mySelect").value;
 
-      if (region == "guatemala" && view) {
+      if (region == "guatemala") {
         view.goTo({
           center: [-91.182087, 14.414071],
           zoom: 7
