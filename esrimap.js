f8017eb5d23c1e187e7a8858d9dcd133bdcc8d5e
Jason Biesinger <36056108+jasonbiesinger@users.noreply.github.com>
2019-05-31 09:22:17 -0600

    Update esrimap.js

--- esrimap.js
+++ esrimap.js
@@ -51,7 +51,7 @@ require([
                     $('#graph').remove();
                 };
 
-                $('div .contentPane').append('<div id="graph"></div>');
+                $('div .esri-popup').append('<div id="graph"></div>');
                 var allLines = data.split('\n');
                 var headers = allLines[0].split(',');
 
@@ -302,7 +302,7 @@ require([
 			},
 			popupTemplate: {
 				title: name_list[i],
-				content: getstreamflow
+				content: content_list[i]
 			}
 		});
 	};
